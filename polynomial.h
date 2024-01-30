07b6a6202626df907105fadc2f6e3220e95eed50
PSewmuthu <apasindusewmuthu@gmail.com>
2024-01-30 22:07:24 +0530

    Create node structure and the type definition of link as pointer to node

--- polynomial.h
+++ polynomial.h
@@ -1,2 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct node *link; // type definition of link as pointer to node
+
+struct node { // node structure
+    int coef; // coefficient of term
+    int pow; // power of term
+    link next; // pointer to next node
+};
