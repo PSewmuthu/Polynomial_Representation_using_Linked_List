7c1c3882854fadad8646e01237fa5ba13fe520a5
PSewmuthu <apasindusewmuthu@gmail.com>
2024-01-30 22:22:37 +0530

    Create insertFront() for insert a node to the front of the linked list

--- polynomial.h
+++ polynomial.h
@@ -24,6 +24,15 @@ link newNode(int coef, int pow){ // create new node
     temp->coef = coef; // set coefficient of new node
     temp->pow = pow; // set power of new node
     temp->next = NULL; // set next pointer of new node to NULL
-    
+
     return temp; // return new node
+}
+
+void insertFront(list *l, link temp){ // insert node at front of list
+    if (l->head == NULL){ // if list is empty
+        l->head = l->tail = temp; // set head and tail to new node
+    } else { // if list is not empty
+        temp->next = l->head; // set next pointer of new node to head
+        l->head = temp; // set head to new node
+    }
 }
