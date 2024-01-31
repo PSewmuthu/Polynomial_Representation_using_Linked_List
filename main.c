5d350347cf1eaf5e16e0f8965bc36305a3dca698
PSewmuthu <apasindusewmuthu@gmail.com>
2024-01-31 23:57:45 +0530

    Create main() and initialize 3 lists

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
 #include "polynomial.h"
+
+int main(){
+    list *l1, *l2, *l3; // declare three lists
+
+    l1 = (list*)malloc(sizeof(list)); // allocate memory for l1
+    l2 = (list*)malloc(sizeof(list)); // allocate memory for l2
+    l3 = (list*)malloc(sizeof(list)); // allocate memory for l3
+
+    initList(l1); // initialize l1
+    initList(l2); // initialize l2
+    initList(l3); // initialize l3
+}
